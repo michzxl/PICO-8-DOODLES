0e692f295ed0d6c5d1b3a8775e625846f07b8317
michzxl <michealh.5678@yahoo.com>
2021-02-24 15:41:07 -0500

    create SOLAR SEA

--- automata/solar_sea.p8
+++ automata/solar_sea.p8
@@ -2,56 +2,98 @@ pico-8 cartridge // http://www.pico-8.com
 version 29
 __lua__
 
+#include vec.lua
+
 local t = 0
 
 function _init()
+	poke(0x5f2e,1)
+	poke(0x5f2d,1)
+
 	cls(2)
+	poke(0x5f5f,0x10)
+	palmix(0b00001111,0,16)
 
 	pal({
-	[0]=0,
+		[0]=0,
 		1+128,
 		1,
 		2,
 		8+128,
 		8,
-		0,
-		0,
-		0,
-		0,
-		0,
-		0,
-		0,
-		0,
-		0,
+		
+		6,
+		7,
+		8,
+		9,
+		10,
+		11,
+		12,
+		13,
+		14,
 		7,
 	}, 1)
+
+	pal({
+		[0]=0,
+		0,
+		1+128,
+		1,
+		2,
+		8+128,
+		--8,
+
+		6,
+		7,
+		8,
+		9,
+		10,
+		11,
+		12,
+		13,
+		14,
+		14,
+	}, 2)
+
+	lines = {}
 end
 
 function _update()
 	t += 1/30
 
+	-- gen lines
+	if rnd(1)<0.04 and #lines<16 then
+		local ln = {}
+		ln.x,ln.y = rnd(128),rnd(32)+96
+		ln.majorlen = 32 + rnd(32) - 16
+		ln.minorlen = 6 + rnd(4) - 2
+		ln.rot = rnd(1)
+		ln.vx,ln.vy = 0,-1 + rnd(0.5) - 0.025
+		ln.vrot = 0.05 * (rnd(1)<0.5 and 1 or -1)
+		ln.rotdamp = 0.95
+		add(lines, ln)
+	end
+
 	for i=1,500 do
 		local x,y = rnd(128),rnd(128)
-		local p = pget(x,y)
 		local c = (sin(x/64) + cos((y+x)/128 - t/8))
 		c = c / 3 + 0.5
-		circ(x,y,1,mid(0,5,c+p))
+		circ(x,y,1,mid(0,5,c+pget(x,y)))
 	end
 
 	for i=1,150 do
 		local x,y = rnd(128),rnd(128)
-		local p = pget(x,y)
-		local p2 = mid(0,p-1,5)
+		local p2 = mid(0,pget(x,y)-1,5)
 		circ(x,y,1,p2)
 	end
 
 	for i=1,150 do
 		local x,y = rnd(128),rnd(128)
-		local p = pget(x,y)
-		circ(x,y,1,mid(0,p+1.5,5))
+		circ(x,y,1,mid(0,pget(x,y)+1.5,5))
 	end
 	
-	for i=1,500 do
+
+	for i=1,500 * min(1, sin(t/16) + 1) do
 		local x,y = rnd(128),rnd(128)
 		local p = pget(x,y)
 		if p~=0 or rnd(1)<0.2 then
@@ -59,10 +101,63 @@ function _update()
 		end
 	end
 
-	local k = 2*sin(t/4)/2+0.5
+	local k = 2.5*sin(t/4)/2+0.5
 	for i=1,100 do
 		local x,y = rnd(128),rnd(128)
 		circ(x,y,1,mid(0,5,k + boxblur(x,y,2)))
+	end
+
+	for i=1,200 do
+		local x,y = rnd(128),rnd(128)
+		circ(x,y,1,0.5 + boxblur(x,y,1))
+	end
+
+	for ln in all(lines) do
+		ln.x = ln.x + ln.vx
+		ln.y = ln.y + ln.vy
+		ln.vrot = ln.vrot * ln.rotdamp
+		ln.rot = ln.rot + ln.vrot
+		ln.rot = alerp(ln.rot, 0.45, 0.035, 1)
+	
+
+		local pos = vec(ln.x,ln.y)
+		local t1 = vec.frompolar(ln.rot, ln.majorlen / 2)
+		local t2 = vec.frompolar(ln.rot + 0.25, ln.minorlen / 2)
+
+		if rnd(1)<0.05 then
+			local ofs = vec(rnd(16)-8)
+			pos += ofs
+			t1 -= ofs
+			t2 += ofs
+		end
+
+		local v1 = pos + t1
+		local v2 = pos - t1
+		
+		local va1,va2 = pos + t1:norm(t1:magn() - ln.minorlen / 2), pos - t1:norm(t1:magn() - ln.minorlen / 2)
+		local wa1 = va1 + t2
+		local wa2 = va1 - t2
+		local wb1 = va2 + t2
+		local wb2 = va2 - t2
+
+		local c1,c2 = 15,3
+		local f1,f2 = 0b0000000000000000, 0b1000010000100001.1
+
+		fillp(f2)
+		line__(v1.x,v1.y,v2.x,v2.y,c2)
+		line__(wa1.x,wa1.y,wa2.x,wa2.y,c2)
+		line__(wb1.x,wb1.y,wb2.x,wb2.y,c2)
+
+		fillp(f1)
+		line2(v1.x,v1.y,v2.x,v2.y,c1)
+		line2(wa1.x,wa1.y,wa2.x,wa2.y,c1)
+		line2(wb1.x,wb1.y,wb2.x,wb2.y,c1)
+
+		fillp()
+
+		if ln.y < 0 - ln.majorlen then
+			del(lines, ln)
+		end
 	end
 end
 
@@ -77,4 +172,49 @@ function boxblur(x,y,width)
 	end
 
 	return sum/count
+end
+
+function line2(x1,y1,x2,y2,c)
+	local num_steps=max(
+	 abs(flr(x2)-flr(x1)),
+	 abs(flr(y2)-flr(y1)))
+	local dx=(x2-x1)/num_steps
+	local dy=(y2-y1)/num_steps
+	for i=0,num_steps do
+	 pset(x1,y1,c)
+	 x1+=dx
+	 y1+=dy
+	end
+  end
+
+function line__(x1,y1,x2,y2,c)
+	local num_steps=max(
+	 abs(flr(x2)-flr(x1)),
+	 abs(flr(y2)-flr(y1)))
+	local dx=(x2-x1)/num_steps
+	local dy=(y2-y1)/num_steps
+	for i=0,num_steps do
+	 circfill(x1,y1,2,c)
+	 x1+=dx
+	 y1+=dy
+	end
+  end
+
+function alerp(a,b,t,p)
+  a=a%p
+  b=b%p
+  if b-a>p/2 then
+	  b=b-p
+  elseif a-b>p/2 then
+	  a=a-p
+  end
+  return a + t*(b - a)
+end
+
+function palmix(bits, region, length)
+	if bits==nil then
+		memset(0x5f70, 0, 16)
+	else
+		memset(0x5f70 + region, bits, length)
+	end
 end
---
+++ automata/vec.lua
@@ -0,0 +1,310 @@
+_const_a = 1007/1024
+_const_b = 441/1024
+_const_c = 5/128
+
+vec = {
+	frompolar = function(ang,r)
+		return vec(r*cos(ang), r*sin(ang))
+	end,
+	unit=function(rot)
+		return 
+			vec(1,0,0),
+			vec(0,1,0),
+			vec(0,0,1)
+	end,
+	one=function()
+		return vec(1,1,1)
+	end,
+	left = function()
+		return vec(-1,0,0)
+	end,
+	right = function()
+		return vec(1,0,0)
+	end,
+	up = function()
+		return vec(0,0,1)
+	end,
+	down = function()
+		return vec(0,0,-1)
+	end,
+	forward = function()
+		return vec(0,-1,0)
+	end,
+	back = function()
+		return vec(0,1,0)
+	end,
+
+	ZERO = 0x0.001,
+
+	copy = function(self)
+		return vec(self.x,self.y,self.z)
+	end,
+
+	polar = function(self)
+		return self:ang(), #self
+	end,
+	ang = function(self)
+		return atan2(self.x,self.y)
+	end,
+	turn = function(self, ang)
+		local v = self:copy()
+		v:setang(self:ang() + ang)
+		return v
+	end,
+
+	setc=function(self, x, y, z)
+		self.x=x or 0
+		self.y=y or 0
+		self.z=z or 0
+		return self
+	end,
+	set=function(self,v)
+		self.x=v.x
+		self.y=v.y
+		self.z=v.z
+		return self
+	end,
+	setang = function(self,ang)
+		return self:setpolar(ang, #self)
+	end,
+	setmagn = function(self,len)
+		return self:set(self:norm(len))
+	end,
+	setpolar = function(self, ang, r)
+		return self:setc(r*cos(ang), r*sin(ang))
+	end,
+
+	magn = function(self)
+		return sqrt(self.x*self.x + self.y*self.y + self.z*self.z)
+	end,
+	sqrmagn = function(v)
+		return v.x*v.x + v.y*v.y + v.z*v.z
+	end,
+
+	-- https://www.flipcode.com/archives/Fast_Approximate_Distance_Functions.shtml
+	-- is only 2D
+	approx_magn = function(v)
+		local mmin = min(v.x,v.y)
+		local mmax = max(v.x,v.y)
+		return _const_a*mmax + _const_b*mmin
+			- (mmax < 16*mmin and _const_c*mmax or 0)
+	end, 
+
+	norm=function(self, len)
+		if self:nearzero(vec.ZERO) then
+			return vec()
+		else
+			return self / self:magn() * (len or 1)
+		end
+	end,
+	perp = function(self, len)
+		return vec(-self.y, self.x):norm(len)
+	end,
+	cross = function(A, B)
+		return vec(
+			A.y*B.z - A.z*B.y,
+			A.z*B.x - A.x*B.z,
+			A.x*B.y - A.y*B.x
+		)
+	end,
+	dot = function(v,w)
+		return v.x*w.x + v.y*w.y + v.z*w.z
+	end,
+	cmult = function(a,b)
+		return vec(
+			a.x*b.x,
+			a.y*b.y,
+			a.z*b.z
+		)
+	end,
+	nonzero = function(self,unit)
+		return not (self.x==0 and self.y==0 and self.z==0)
+	end,
+	nearzero = function(self,dist)
+		return #self<dist
+	end,
+
+	constrain = function(self,anchor,dist)
+		return (self - anchor):norm(dist) + anchor
+	end,
+	constrain_min = function(self,anchor,dist)
+		local v = self - anchor
+		return #v<dist and self:constrain(anchor,dist) or self
+	end,
+	constrain_max = function(self,anchor,dist)
+		local v = self - anchor
+		return #v>dist and self:constrain(anchor,dist) or self
+	end,
+
+	xy = function(self)
+		return self.x,self.y
+	end,
+	xyz=function(self)
+		return self.x,self.y,self.z
+	end,
+	cx=function(self)
+		return vec(self and self.x or 1,0,0)
+	end,
+	cy=function(self)
+		return vec(0,self and self.y or 1,0)
+	end,
+	cz=function(self)
+		return vec(0,0,self and self.z or 1)
+	end,
+	distr = function(self,f)
+		return vec(
+			f(self.x),
+			f(self.y),
+			f(self.z)
+		)
+	end,
+	zip = function(self,v,f)
+		return vec(
+			f(self.x,v.x),
+			f(self.y,v.y),
+			f(self.y,v.y)
+		)
+	end,
+	fflr = function(self, unit)
+		return self:distr(function(a) 
+			return fflr(a, unit or 1)
+		end)
+	end,
+	fflrz = function(self, unit)
+		local signs = self:distr(sgn)
+		local fv = self:distr(abs):fflr(unit)
+		return fv:cmult(signs)
+	end,
+
+	rot_x = function(v, ang)
+		return vec(
+			v.x, 
+			v.y * cos(ang) - v.z * sin(ang), 
+			v.y * sin(ang) + v.z * cos(ang)
+		)
+	end,
+	rot_y = function(v, ang)
+		return vec(
+			v.z * sin(ang) + v.x * cos(ang), 
+			v.y, 
+			v.z * cos(ang) - v.x * sin(ang)
+		)
+	end,
+	rot_z = function(v, ang)
+		return vec(
+			v.x * cos(ang) - v.y * sin(ang), 
+			v.x * sin(ang) + v.y * cos(ang), 
+			v.z
+		)
+	end,
+
+	rot_yxz = function(v, a)
+		local nv = v:copy()
+		nv = vec.rot_y(nv, a.y)
+		nv = vec.rot_x(nv, a.x)
+		nv = vec.rot_z(nv, a.z)
+		return nv
+	end,
+	rot_zxy = function(v,angs)
+		local nv = v:copy()
+		nv = vec.rot_z(nv, angs.z)
+		nv = vec.rot_x(nv, angs.x)
+		nv = vec.rot_y(nv, angs.y)
+		return nv
+	end,
+
+	u_rot_yxz = function(angs)
+		local cx,sx,cy,sy,cz,sz = angs:cache_trig()
+		local x,y,z = angs:xyz()
+		local ux = vec(
+			cy*cz - sy*sx*sz,
+			cy*sz + sy*sx*cz,
+			-sy*cx
+		)
+		local uy = vec(
+			-cx*sz,
+			cx*cz,
+			sx
+		)
+		local uz = vec(
+			sy*cz + cy*sx*sz,
+			sy*sz - cy*sx*cz,
+			cy*cx
+		)
+		return vec(ux,uy,uz)
+	end,
+	u_rot_zxy = function(angs)
+		local cx,sx,cy,sy,cz,sz = angs:cache_trig()
+		local ux = vec(
+			cy*cz + sx*sy*sz,
+			cx*sz,
+			-cz*sy + cy*sx*sz
+		)
+		local uy = vec(
+			cz*sx*sy - cy*sz,
+			cx*cz,
+			cy*cz*sx + sy*sz
+		)
+		local uz = vec(
+			cx*sy,
+			-sx,
+			cx*cy
+		)
+		return vec(ux,uy,uz)
+	end,
+
+	cache_trig = function(angs)
+		return 
+			cos(angs.x),sin(angs.x),
+			cos(angs.y),sin(angs.y),
+			cos(angs.z),sin(angs.z)
+	end,
+
+	eq = function(a,b)
+		return a.x==b.x and a.y==b.y and a.z==b.z
+	end,
+
+	swap = function(v, c1, c2)
+		local nv = v:copy()
+		local tmp = v[c1]
+		v[c1] = v[c2]
+		v[c2] = tmp
+		return nv
+	end,
+}
+
+_vec={
+	__index=vec,
+	__add=function(p1,p2)
+		return vec(p1.x+p2.x,p1.y+p2.y,p1.z+p2.z)
+	end,
+	__sub=function(p1,p2)
+		return vec(p1.x-p2.x,p1.y-p2.y,p1.z-p2.z)
+	end,
+	__mul=function(p,a)
+		if type(a)=="vec" then
+			return vec(p.x*a.x,p.y*a.y,p.z*a.z)
+		else
+			return vec(p.x*a,p.y*a,p.z*a)
+		end
+	end,
+	__div=function(p,a)
+		return vec(p.x/a,p.y/a,p.z/a)
+	end,
+	__unm=function(p)
+		return vec(-p.x,-p.y,-p.z)
+	end,
+	__len = vec.magn,
+	__tostring=function(self)
+		return "<"..flr(100*self.x)/100 ..","..flr(100*self.y)/100 ..","..flr(100*self.z)/100 ..">"
+	end,
+}
+
+setmetatable(vec, {
+	__call = function(self,x,y,z)
+		return setmetatable({
+			x=x or 0,
+			y=y or 0,
+			z=z or 0,
+		}, _vec)
+end})
